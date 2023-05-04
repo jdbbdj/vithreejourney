41088a0eeef56cbcf90c8973a5db568283079039
jamesdarylbertiz@gmail.com <75722677+jdbbdj@users.noreply.github.com>
2023-05-04 19:39:39 +0800

    Animations: GSAP Library

--- ThreeComponents/index.ts
+++ ThreeComponents/index.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+import { gsap } from 'gsap'
 //Global UTILS
 
 //assets
