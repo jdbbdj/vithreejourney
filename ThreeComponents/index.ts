2e92996dd0b81aeb4bfb854cd8d63918739bf0f2
jamesdarylbertiz@gmail.com <75722677+jdbbdj@users.noreply.github.com>
2023-05-05 01:21:16 +0800

    Cameras: Perspective cameras: near, far, fov, aspect ratio; CameraHelpers

--- ThreeComponents/index.ts
+++ ThreeComponents/index.ts
@@ -20,13 +20,6 @@ export default class ThreeModel {
 
         const scene = new THREE.Scene()
 
-        const camera = new THREE.PerspectiveCamera(
-            75,
-            window.innerWidth / window.innerHeight,
-            0.1,
-            1000
-        )
-
         const renderer = new THREE.WebGLRenderer()
         renderer.setSize(window.innerWidth, window.innerHeight)
         document.body.appendChild(renderer.domElement)
@@ -36,15 +29,28 @@ export default class ThreeModel {
         const cube = new THREE.Mesh(geometry, material)
         scene.add(cube)
 
-        camera.position.z = 5
+        const camera = new THREE.PerspectiveCamera(
+            75,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            1000
+        )
 
+        const camera2 = new THREE.PerspectiveCamera(
+            75,
+            window.innerWidth / window.innerHeight,
+            0.51,
+            0.75
+        )
+        camera2.position.z = 2
+        camera2.rotation.y = Math.PI / 2
+        camera.position.set(0, 0, 5)
+        scene.add(camera2)
+        const helper = new THREE.CameraHelper(camera2)
+        scene.add(helper)
         function animate() {
-            requestAnimationFrame(animate)
-
-            cube.rotation.x += 0.01
-            cube.rotation.y += 0.01
-
             renderer.render(scene, camera)
+            requestAnimationFrame(animate)
         }
 
         animate()
