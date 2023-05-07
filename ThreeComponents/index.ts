04a399156debe6c1cae1507c1e4c67f8e9b44e43
jamesdarylbertiz@gmail.com <75722677+jdbbdj@users.noreply.github.com>
2023-05-07 21:37:56 +0800

    Debug: Initialize

--- ThreeComponents/index.ts
+++ ThreeComponents/index.ts
@@ -41,9 +41,6 @@ export default class ThreeModel {
         function animate() {
             requestAnimationFrame(animate)
 
-            cube.rotation.x += 0.01
-            cube.rotation.y += 0.01
-
             renderer.render(scene, camera)
         }
 
